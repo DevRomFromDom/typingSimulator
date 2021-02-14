8ae6e127ca64f7ad3dff5265df207bcc2ecdf697
DevilR <romasan888888@gmail.com>
2021-02-14 15:42:36 +0300

    View works. Add keydown events.

--- src/Main/Text/Text.jsx
+++ src/Main/Text/Text.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Text.module.scss';
-import Word from './Word/Word';
+import Letter from './Letter/Letter';
 
 const Text = () => {
-    const [text, setText] = useState('');
-    const [activeLetter, setActiveLetter] = useState(0);
-    const [activeWord, setActiveWord] = useState(0);
-    const getText = async () => {
-        setActiveWord(0);
-        setText('');
+    const [text, setText] = useState([]);
+    const [passedText, setPassedText] = useState([]);
+    const [activeText, setActiveText] = useState([]);
+    const [defaultText, setDefaultText] = useState([]);
+    const [changeIndex, setChangeIndex] = useState(0);
+    const [status, setStatus] = useState('active');
+    const [correctCount, setCorrectCount] = useState(0);
+    const [mistakeCount, setMistakeCount] = useState(0);
+    const inputRef = useRef();
+
+    const getText = async (e) => {
         try {
             const newText = async () => {
                 const res = await fetch('https://fish-text.ru/get?&type=paragraph&number=3&type=json');
@@ -17,51 +22,82 @@ const Text = () => {
             };
             newText().then((data) => {
                 console.log(data.text);
-                setText(data.text);
+                setText(data.text.split(''));
+                setChangeIndex(0);
+                setStatus('active');
+                inputRef.current.focus();
             });
         } catch (e) {
             console.log('error');
         }
     };
-    const changeWord = () => {
-        setActiveWord(activeWord + 1);
-    };
+    useEffect(() => {
+        const useKeyPress = (e) => {
+            inputRef.current.focus();
+            if (e.key === activeText[0]) {
+                changeLetter();
+            }
+            if (e.key !== activeText[0] && text !== []) {
+                mistake();
+            } 
+        };
+        const useTabDown = (e) => {
+            if (e.key === 'Tab') {
+                e.preventDefault();
+                inputRef.current.focus();
+            }
+        };
+        document.addEventListener('keypress', useKeyPress);
+        document.addEventListener('keydown', useTabDown);
+        return () => {
+            document.removeEventListener('keypress', useKeyPress);
+            document.removeEventListener('keydown', useTabDown);
+        };
+    }, [activeText]);
+
+    useEffect(() => {
+        setPassedText(text.slice(0, changeIndex));
+        setActiveText(text.slice(changeIndex, changeIndex + 1));
+        setDefaultText(text.slice(changeIndex + 1));
+    }, [text, changeIndex]);
+
     const changeLetter = () => {
-        setActiveLetter(activeLetter + 1);
-        if (activeLetter > arrText[activeWord].length-1) {
-            console.log('x');
-            setActiveWord(activeWord + 1);
-            setActiveLetter(0);
-        }
+        setCorrectCount(correctCount + 1);
+        setChangeIndex(changeIndex + 1);
+        setStatus('active');
     };
 
-    const arrText = text.split(' ');
-    const act = arrText[activeWord];
-    console.log(act, act.length);
-
-    // console.log(arrText);
+    const mistake = () => {
+        setStatus('mistake');
+        setMistakeCount(mistakeCount + 1);
+    };
     return (
         <div>
+            <button onClick={getText}>Get text</button>
+            <input type="text" ref={inputRef} className={styles.input} />
             <div className={styles.text}>
-                {text !== '' ? (
-                    arrText.map((el, index) => {
-                        return (
-                            <Word
-                                value={el + ' '}
-                                key={index}
-                                indexWord={index}
-                                activeWord={activeWord}
-                                activeLetter={activeLetter}
-                            />
-                        );
+                {passedText !== '' ? (
+                    passedText.map((el, index) => {
+                        return <Letter value={el} key={index} style={'passed'} />;
                     })
                 ) : (
-                    <div>No text</div>
+                    <div></div>
+                )}
+                {activeText !== '' ? (
+                    activeText.map((el, index) => {
+                        return <Letter value={el} key={index} style={status} />;
+                    })
+                ) : (
+                    <div></div>
+                )}
+                {defaultText !== '' ? (
+                    defaultText.map((el, index) => {
+                        return <Letter value={el} key={index} style={'default'} />;
+                    })
+                ) : (
+                    <div></div>
                 )}
             </div>
-            <button onClick={getText}>Get text</button>
-            <button onClick={changeWord}>change word</button>
-            <button onClick={changeLetter}> change letter</button>
         </div>
     );
 };
